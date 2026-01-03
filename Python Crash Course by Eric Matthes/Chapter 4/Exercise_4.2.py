6ccc6c54722f197ed960043bb95fd199860c009f
Margaret Adeniran <mtoluwani.adeniran@gmail.com>
2026-01-03 02:07:00 +0100

    Updates

---
+++ Python Crash Course by Eric Matthes/Chapter 4/Exercise_4.2.py
@@ -0,0 +1,5 @@
+pets = ['dog', 'parrot', 'chicken', 'rabbit']
+for pet in pets:
+  print(f"a {pet} would make a great pet.")
+
+print("Any of these animals would make a great pet!")
---
+++ Python Crash Course by Eric Matthes/Chapter 4/Exercise_4.6.py
@@ -0,0 +1,3 @@
+odds = list(range(1,20,2))
+for number in odds:
+  print(number)
