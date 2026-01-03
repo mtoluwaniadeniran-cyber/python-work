6965b8c595aefab798122be43559292d7ac4d851
Margaret Adeniran <mtoluwani.adeniran@gmail.com>
2026-01-03 21:36:02 +0100

    Updates

---
+++ Python Crash Course by Eric Matthes/Chapter 5/Exercise_5.1.py
@@ -0,0 +1,15 @@
+car = 'subaru'
+print ("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+print("\n")
+
+car = 'Tesla'
+car.lower() == 'tesla'
+print("Is car.lower() == 'tesla'?")
+print(car == 'Tesla')
+
+print("\nIs car == 'Cyber truck'?")
+print(car == 'Cyber Truck')
---
+++ Python Crash Course by Eric Matthes/Chapter 5/toppings.py
@@ -0,0 +1,5 @@
+requested_topping = ['mushrooms', 'onions', 'pineapple']
+'mushrooms' in requested_topping
+
+'''if requested_topping != ']anchovies':
+    print("Hold the anchovies!")'''
