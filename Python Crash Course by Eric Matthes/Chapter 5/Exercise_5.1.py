aeb4a8b99d9cf20d113436e51ed6d3ce88e0614a
Margaret Adeniran <mtoluwani.adeniran@gmail.com>
2026-01-04 21:06:53 +0100

    Updates

--- Python Crash Course by Eric Matthes/Chapter 5/Exercise_5.1.py
+++ Python Crash Course by Eric Matthes/Chapter 5/Exercise_5.1.py
@@ -13,3 +13,28 @@ print(car == 'Tesla')
 
 print("\nIs car == 'Cyber truck'?")
 print(car == 'Cyber Truck')
+
+perfumes = ['Dior', 'Chanel', 'Gucci', 'Versace', 'Prada']
+print("\nIs 'Dior' in perfumes?")
+print('Dior' in perfumes)
+
+print("\nIs 'Armani' in perfumes?")
+print('Armani' in perfumes)
+
+car = 'bmw'
+car == 'BMW'
+print("\nIs car == 'BMW'?")
+print(car == 'BMW')
+
+season = 'Summer'
+season.lower() == 'summer'
+print("\nIs season.lower() == 'summer'?")
+print(season == 'Summer')
+
+skincare_brand = 'The Ordinary'
+if skincare_brand != 'Simple':
+    print("\nIs skincare_brand != 'Simple'?")
+    print(skincare_brand != 'Simple')
+    
+print("\nIs skincare_brand != 'The Ordinary'?")
+print(skincare_brand != 'The Ordinary')
---
+++ Python Crash Course by Eric Matthes/Chapter 5/Exercise_5.2.py
@@ -0,0 +1,26 @@
+game = 'subway surfers'
+game == 'Minecraft'
+print(game == 'Minecraft')
+print(game == 'subway surfers')
+
+print("\n")
+dress = 'Evening Gown'
+dress.lower() == 'evening gown'
+print(dress.lower() == 'Evening Gown')
+print(dress.lower() == 'evening gown')
+
+print("\n")
+age_1 = 20
+age_2 = 18
+age_3 = 10
+print(age_1 > age_2)
+print(age_2 < age_3)
+
+print("\n")
+print(age_1>=age_2 or age_2<=age_3)
+print(age_1<=age_2 and age_2>=age_3)
+
+print("\n")
+pets = ['dog', 'cat', 'rabbit']
+print('cat' in pets)
+print('dog' not in pets)
