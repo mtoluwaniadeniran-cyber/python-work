50f8bcac98e66af1cd14b48d275866e9209e8da4
Margaret Adeniran <mtoluwani.adeniran@gmail.com>
2026-01-14 09:38:37 +0100

    Updates

---
+++ Python Crash Course by Eric Matthes/Chapter 6/Exercise_6.11.py
@@ -0,0 +1,7 @@
+cities = {'Country': 'Japan', 'Population': '37 million', 'Fact': "One of the world's safest cities.",
+          'Country': 'India', 'Population': '30-35 million', 'Fact': 'Home to the Qutub Minar, a UNESCO World Heritage site.',
+          'Country': 'China', 'Population': '26-31 million', 'Fact': "Features the world's first and fastest commercial maglev train line."
+        }
+
+for key, value in cities:
+    print("cities.item()")
---
+++ Python Crash Course by Eric Matthes/Chapter 7/greeter.py
@@ -0,0 +1,9 @@
+'''name = input("Please enter your name: ")
+print(f"\nHello, {name}!")'''
+
+#Assigning your prompt to a variable
+prompt = "If you share your name, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print(f"\nHello, {name}!")
