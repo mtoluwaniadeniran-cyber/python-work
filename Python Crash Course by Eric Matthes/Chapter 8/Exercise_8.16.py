4d9e9b43a2da0c9c36049ba51e1e2f5776ab31a8
Covenant University <madeniran.2300324@stu.cu.edu.ng>
2026-03-04 07:11:33 +0100

    Updates

--- Python Crash Course by Eric Matthes/Chapter 8/Exercise_8.16.py
+++ Python Crash Course by Eric Matthes/Chapter 8/Exercise_8.16.py
@@ -3,3 +3,15 @@ import make_sandwiches
 make_sandwiches.make_sandwich('butter', 'sardine')
 make_sandwiches.make_sandwich('jam', 'peanut butter')
 make_sandwiches.make_sandwich('tomato', 'lettuce', 'tofu')
+
+from make_sandwiches import make_sandwich
+make_sandwich('avocado', 'spinach', 'mayo')
+
+from make_sandwiches import make_sandwich as ms
+ms('turkey', 'bacon', 'lettuce', 'tomato', 'mayo')
+
+import make_sandwiches as ms
+ms.make_sandwich('ham', 'cheese', 'mustard')
+
+from make_sandwiches import *
+make_sandwich('roast beef', 'horseradish', 'lettuce', 'tomato')
---
+++ Python Crash Course by Eric Matthes/Chapter 8/make_sandwiches.py
@@ -0,0 +1,4 @@
+#This is Exercise_8.16
+def make_sandwich(*condiments):
+    """Print the list of condiments that have been requested."""
+    print(condiments)
